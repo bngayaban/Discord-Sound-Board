34c505a5949ad456ba1ee11d406ee905e4fb5e5e
Unknown <bngayaban@ucdavis.edu>
2019-11-02 23:25:05 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Discord Sound Board
+# Requirements
+
+visual studio build tools 2015
+
+https://stackoverflow.com/questions/54658125/discord-bot-fix-ffmpeg-not-found
+FFMPEG, install with chocaltely using choco install ffmpeg
+    then in node install using npm install ffmpeg
+
+node-opus, something else you need
---
+++ app.js
@@ -0,0 +1,60 @@
+// Require discord.js package
+const Discord = require("discord.js");
+
+// Create a new client using the new keyword
+const client = new Discord.Client();
+
+// Accessing the token ./token.json
+const {
+    token
+} = require("./token.json");
+
+// Display a message once the bot has started
+client.on("ready", () =>{
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// When the bot is reconnecting to the websocket
+client.on("reconnecting", () =>{
+    console.log(`This bot is reconnecting: ${client.user.tag}`);
+});
+
+// When the bot disconnects from the websocket
+client.on("disconnect", () =>{
+    console.log(`This bot is now disconnected: ${client.user.tag}`);
+});
+
+// !help command, message event + message object
+client.on("message", msg => {
+    const msgToLower = msg.content.toLowerCase();
+    const helpCommand = "!help";
+    const soundBoardPrefix = "!sb"
+    const voiceChannel = msg.member.voiceChannel;
+
+    if(!msg.content.startsWith(soundBoardPrefix) || msg.author.bot) return;
+
+    if(!voiceChannel)
+    {
+        return msg.reply('you need to be in a voice channel to use.');
+    }
+
+    const args = msg.content.slice(soundBoardPrefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    //attempt to connect to voice channel
+    voiceChannel.join().then(connection =>
+        {
+            const dispatcher = connection.playFile('./Audio/StillAlive.mp3');
+            dispatcher.on("end", end => {
+                voiceChannel.leave();
+                });
+            }).catch(err => console.log(err));
+    if (msgToLower === helpCommand){
+        msg.reply("This bot doesn't use any command at all, apart from the !help command.");
+    }
+});
+
+
+
+// Log in the bot with the token
+client.login(token);
