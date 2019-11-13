caa27d69855eb60c4d3393aae50d5157635e90b3
Bryan <bngayaban@ucdavis.edu>
2019-11-13 00:27:18 -0800

    insert works

--- database.js
+++ database.js
@@ -2,49 +2,48 @@ const sqlite3 = require('sqlite3').verbose();
 const folder = './Audio/';
 const fs = require('fs');
 
-function dbInit()
-{
-    let db = new sqlite3.Database('./Database/audio.db', (err) => {
-        if(err){
-            console.error(err.message);
-        }
-        else{
-            try {
-                db.run ('CREATE TABLE IF NOT EXISTS audio(fileName TEXT, tags TEXT)', (err) =>{
-                    if(err){
-                        console.error(err.message);
-                    }
-                    else{
-                        db.run("BEGIN TRANSACTION");
-                        fs.readdirSync(folder).forEach(file => {
-                            console.log(file);
-                            db.run('INSERT INTO audio(fileName) VALUES ' + file, (err) => {
-                                if(err){
-                                    console.error(JSON.stringify(err));
-                                    return;
-                                }
-                            });
-                            })
-                        db.run("END");
-                    }
-                })
-            
-            }
-            catch(e)
-            {
-                console.error(e);
-            }
-            
-        }
-        console.log('Connected to the audio database.');
-    });
-
-    
-        
+const db = new sqlite3.Database('./Database/audio.db', (err) => {
+    if(err){
+        console.error(err.message);
+    }
+});
 
 
-    
+function dbInitialize(){
+    const sql = "CREATE TABLE IF NOT EXISTS audio(fileName TEXT, tags TEXT, UNIQUE(fileName))";
 
-    db.close();
+    db.run(sql, dbPopulate);
 }
-dbInit();
+
+function dbPopulate(err){
+    let sql = "INSERT OR IGNORE INTO audio (fileName) VALUES ";
+    if(err) {
+        console.error(err.message);
+    }
+    else {
+        const files = fs.readdirSync(folder);
+        const placeholders = files.map((file) => '(?)').join(',');
+        
+        sql += placeholders;
+        db.run(sql, files, (err) => {
+            if(err) {
+                return console.error(err.message);
+            }
+            console.log(`Rows inserted ${this.changes}`);
+        });
+    }
+
+}
+
+function dbRead(fileName) {
+    const sql = `SELECT fileName FROM audio WHERE fileName = (?) OR tags = (?) LIMIT 1`;
+
+    db.get(sql, [fileName, fileName], (err) => {
+        if(err) {
+            return console.error(err.message);
+        }
+        console.log(`Rows inserted ${this.changes}`);
+    });
+}
+
+dbInitialize();
