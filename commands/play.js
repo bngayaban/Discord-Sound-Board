931ba7b9e5c607bb6f6f209b079d1fa0f7760d34
Bryan <bngayaban@ucdavis.edu>
2020-04-06 14:36:50 -0700

    Added time out and configure timeout length

--- commands/play.js
+++ commands/play.js
@@ -1,8 +1,6 @@
 const Database = require("../dbObjects.js");
 const { Op } = require("sequelize");
-const {audioDirectory} = require('../config.js');
-
-
+const {audioDirectory, timeoutTime} = require('../config.js');
 
 function ordinalInt(n) {
     return [,'st','nd','rd'][n%100>>3^1&&n%10]||'th';
@@ -12,26 +10,30 @@ async function play(connection, messageChannel, voiceChannel, server) {
     const second = 1000; //milliseconds
     const minute = 60 * second;
 
+    // Make sure stream doesn't end early
+    if(server.timeout) clearTimeout(server.timeout);
+
+    //Play song, then modify queue
     server.dispatcher = connection.play(`${audioDirectory}${server.queue[0]}`);
     server.nowPlaying = server.queue[0];
     server.queue.shift();
     
     server.dispatcher.on("finish", end => {
-        console.log("setting timeout");
-        const timeout = setTimeout(() => {
-            server.nowPlaying = "";
-            connection.disconnect();
-            },
-            0.1 * minute);
         if(server.queue[0]) {
             play(connection, messageChannel, voiceChannel, server);
-            clearTimeout(timeout);
+        } else {
+            console.log("setting timeout");
+            server.nowPlaying = "";
+            server.timeout = setTimeout(() => {
+                connection.disconnect();
+                },
+                timeoutTime * minute);
         }
     });
 }
 
 async function playSong(message, args, servers) {
-    let sfx = args;
+    const sfx = args;
     const sfxQuery = await Database.findOne({where:{[Op.or]: [{fileName: sfx}, {tags: sfx}]} });
 
     if(!servers[message.guild.id]) {
@@ -40,20 +42,22 @@ async function playSong(message, args, servers) {
         };
     } //creates server for containing song queue and nowPlaying
 
-    var server = servers[message.guild.id];
+    const server = servers[message.guild.id];
 
     if(sfxQuery) {
         server.queue.push(sfxQuery.get('fileName'));
         console.log(server.queue);
-        if(server.nowPlaying)
-            message.channel.send(`Sound ${sfx} added as ${server.queue.length}${ordinalInt(server.queue.length)} in queue.`);
+        
+        if(server.nowPlaying) {
+            return message.channel.send(`Sound ${sfx} added as ${server.queue.length}${ordinalInt(server.queue.length)} in queue.`);
+        }
 
-            //console.log(message.channel.guild.voice);
-
-        if(!message.channel.guild.voice || !message.channel.guild.voice.connection)// first way around need to check if voice exists, afterwards need to check if it's connected
+        if(message.member.voice.channel) {
             message.member.voice.channel.join().then(connection => {
                 play(connection, message.channel, message.member.voice.channel, server);
             }).catch(err => console.log(err));
+        }
+            
         return;
     }
     else {
--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 const {token} = require('./token.json');
 const prefix = '!sb';
 const audioDirectory = './Audio/';
+const timeoutTime = 5; //in minutes
 
 module.exports = {
     token,
     prefix,
-    audioDirectory
+    audioDirectory,
+    timeoutTime
 };
