e8a31d42eacacf088d40dd9be6b9c50b215d9628
Bryan <bngayaban@ucdavis.edu>
2020-04-06 02:30:51 -0700

    changed list to work with really long lists

--- commands/list.js
+++ commands/list.js
@@ -3,8 +3,8 @@ const Database = require("../dbObjects.js");
 async function listSongs(message, args) {
     const songs = await Database.findAll({attributes: ['tags']});
     const songList = songs.map(s => s.tags).join(', ') || "No songs.";
-
-    return message.channel.send(`List of songs: ${songList}`);
+    console.log(songList);
+    return message.channel.send(`List of songs: ${songList}`, {split:{char:', '}});
 }
 
 module.exports = {
