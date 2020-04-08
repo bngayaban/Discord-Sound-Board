f8f2836582115daea7a19565c97f8e46d3ae1ce4
Bryan <bngayaban@ucdavis.edu>
2020-04-08 16:39:40 -0700

    Play supports reading from multiple directories

--- commands/play.js
+++ commands/play.js
@@ -1,16 +1,16 @@
-const Database = require("../dbObjects.js");
+const {Audio, FileLocation} = require("../dbObjects.js");
 const { Op } = require("sequelize");
-const {audioDirectory} = require('../config.js');
-
-
+const {join} = require('path');
 
 function ordinalInt(n) {
     return [,'st','nd','rd'][n%100>>3^1&&n%10]||'th';
 } //https://stackoverflow.com/a/39466341
 
 async function play(connection, messageChannel, voiceChannel, server) {
-    server.dispatcher = connection.play(`${audioDirectory}${server.queue[0]}`);
-    server.nowPlaying = server.queue[0];
+    const [song, directory] = server.queue[0];
+
+    server.dispatcher = connection.play(join(directory, song));
+    server.nowPlaying = song;
     server.queue.shift();
     
     server.dispatcher.on("finish", end => {
@@ -25,23 +25,22 @@ async function play(connection, messageChannel, voiceChannel, server) {
 
 async function playSong(message, args, servers) {
     let sfx = args;
-    const sfxQuery = await Database.findOne({where:{[Op.or]: [{fileName: sfx}, {tags: sfx}]} });
-
+    const sfxQuery = await Audio.findOne({where:{[Op.or]: [{fileName: sfx}, {tags: sfx}]}, include: FileLocation });
     if(!servers[message.guild.id]) {
         servers[message.guild.id] = {
             queue: [],
         };
     } //creates server for containing song queue and nowPlaying
 
-    var server = servers[message.guild.id];
+    const server = servers[message.guild.id];
 
     if(sfxQuery) {
-        server.queue.push(sfxQuery.get('fileName'));
+        server.queue.push([sfxQuery.get('fileName'), sfxQuery.audioDirectory.filePath]);
         console.log(server.queue);
         if(server.nowPlaying)
             message.channel.send(`Sound ${sfx} added as ${server.queue.length}${ordinalInt(server.queue.length)} in queue.`);
 
-            console.log(message.channel.guild.voice);
+            //console.log(message.channel.guild.voice);
 
         if(!message.channel.guild.voice || !message.channel.guild.voice.connection)// first way around need to check if voice exists, afterwards need to check if it's connected
             message.member.voice.channel.join().then(connection => {
