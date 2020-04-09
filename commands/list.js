de533c0e63592017bc71f5c84a4095e240f5cc92
Bryan <bngayaban@ucdavis.edu>
2020-04-08 17:53:22 -0700

    List and play use new DB

--- commands/list.js
+++ commands/list.js
@@ -1,7 +1,7 @@
-const Database = require("../dbObjects.js");
+const {Audio} = require("../dbObjects.js");
 
 async function listSongs(message, args) {
-    const songs = await Database.findAll({attributes: ['tags']});
+    const songs = await Audio.findAll({attributes: ['tags']});
     const songList = songs.map(s => s.tags).join(', ') || "No songs.";
     console.log(songList);
     return message.channel.send(`List of songs: ${songList}`, {split:{char:', '}});
