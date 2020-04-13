29763c4a6e148f8c62ce5061bb87f831cf357a8e
Bryan <bngayaban@ucdavis.edu>
2020-04-13 16:27:17 -0700

    added nickname support to add

--- commands/add.js
+++ commands/add.js
@@ -8,9 +8,19 @@ async function add(message, args) {
     const attachment = message.attachments.first(); //assume only 1 attachment
     const url = attachment.url
     const fileName = url.split('/').pop();
+    let nickname = args[0].toLowerCase();
+
     console.log(url);
     
     if(!url) return message.channel.send("No attachment found.");
+    if(nickname) {
+        const query = await Audio.findOne({where:{tags: nickname}});
+
+        if(query) {
+            message.channel.send(`${nickname} already exists in database. Using default instead.`);
+            nickname = fileName.split('.').slice(0, -1).join('.').toLowerCase();
+        }    
+    }
 
     await download(fileName, url)
         .then(() => {
@@ -21,9 +31,9 @@ async function add(message, args) {
             message.channel.send(`Error: ${error}`);
         });
 
-    await updateDB(fileName);
+    await updateDB(fileName, nickname);
 
-    return message.channel.send(`${fileName} added as ${fileName.split('.').slice(0, -1).join('.').toLowerCase()}`);
+    return message.channel.send(`${fileName} added as ${nickname}`);
 }
 
 //https://github.com/milutinke/Discord-Server-Channel-Attachment-Downloader-BOT/blob/master/download.js
@@ -58,10 +68,10 @@ async function download(fileName, url) {
     });
 }
 
-async function updateDB(file) {
+async function updateDB(file, nickname) {
     const entry = await Audio.create({
         fileName: file,
-        tags: file.split('.').slice(0, -1).join('.').toLowerCase()
+        tags: nickname
     });
 
     await entry.setAudioDirectory(1);
@@ -70,7 +80,8 @@ async function updateDB(file) {
 module.exports = {
     name: 'add',
     description: 'Add a sound file to be played',
-    usage: '<prefix><command name> <file to add>',
+    usage: '<prefix><command name> <file to add> <optional nickname>',
+    optionalArgs: 1,
     attachments: true,
     execute(message, args) {
         return add(message, args);
