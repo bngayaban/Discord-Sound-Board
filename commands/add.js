07714a542d31f2c6126b425de20d84cdfb9da760
Bryan <bngayaban@ucdavis.edu>
2020-04-15 13:05:56 -0700

    Added checks to add file command

--- commands/add.js
+++ commands/add.js
@@ -8,51 +8,60 @@ async function add(message, args) {
     const attachment = message.attachments.first(); //assume only 1 attachment
     const url = attachment.url
     const fileName = url.split('/').pop();
-    let nickname = args[0].toLowerCase();
+    let nickname = args[0];
 
     console.log(url);
     
+    // check if attachment exists
     if(!url) return message.channel.send("No attachment found.");
+    //check if file exists
+    if(fs.existsSync(`${audioDirectories[0]}/${fileName}`)) {
+        return message.channel.send(`${fileName} already exists please rename and try again.`);
+    }
+    //check if nickname exists in database
     if(nickname) {
+        nickname = nickname.toLowerCase();
         const query = await Audio.findOne({where:{tags: nickname}});
-
         if(query) {
             message.channel.send(`${nickname} already exists in database. Using default instead.`);
             nickname = fileName.split('.').slice(0, -1).join('.').toLowerCase();
-        }    
+        }
+    } else {
+        nickname = fileName.split('.').slice(0, -1).join('.').toLowerCase();
     }
-
-    await download(fileName, url)
-        .then(() => {
-            console.log(`Successfully Downloaded: ${url}`)
-        })
-        .catch(error => {
-            console.log(`Failed to download: ${url}\n${error}`)
-            message.channel.send(`Error: ${error}`);
-        });
-
-    await updateDB(fileName, nickname);
-
-    return message.channel.send(`${fileName} added as ${nickname}`);
+    
+    try {
+        // start downloading after checks
+        await download(fileName, url);
+        console.log(`Successfully Downloaded: ${url}`);
+        
+        //update database with new file and nickname
+        await updateDB(fileName, nickname);
+        return message.channel.send(`${fileName} added as ${nickname}`);
+    } catch (error) {
+        return message.channel.send(`Error: ${error}`);
+    }
 }
 
 //https://github.com/milutinke/Discord-Server-Channel-Attachment-Downloader-BOT/blob/master/download.js
 async function download(fileName, url) {
     const audioDir = path.dirname(__dirname);
     const dirPath = path.resolve(audioDir, audioDirectories[0]);
+    const megabyte = 1000000;
     console.log(dirPath)
     if(!fs.existsSync(dirPath))
         fs.mkdirSync(dirPath);
 
     const filePath = path.join(dirPath, `${fileName}`);
     console.log(filePath)
-
-    if(fs.existsSync(filePath)) {
+    
+    const headers = await axios.head(url);
+    if(headers.headers['content-length'] > 6 * megabyte) {
         return new Promise((resolve, reject) => {
-            reject('File with same name already in system.');
+            reject(Error(`File can not exceed ${6 * megabyte} megabytes.`))
         })
     }
-       
+
     const writer = fs.createWriteStream(filePath);
     const response = await axios({
         url,
@@ -69,12 +78,15 @@ async function download(fileName, url) {
 }
 
 async function updateDB(file, nickname) {
-    const entry = await Audio.create({
-        fileName: file,
-        tags: nickname
-    });
-
-    await entry.setAudioDirectory(1);
+    try {
+        const entry = await Audio.create({
+            fileName: file,
+            tags: nickname
+        });
+        await entry.setAudioDirectory(1);
+    } catch (error) {
+        return Promise.reject(new Error(`Failed to add: ${file}\n${error}`));
+    }   
 }
 
 module.exports = {
