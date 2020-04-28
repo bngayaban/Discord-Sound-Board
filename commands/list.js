23301e4c9ce4ab2af8fcd86b9ac37d8f565d62ac
Bryan <bngayaban@ucdavis.edu>
2020-04-27 20:08:08 -0700

    List sorts alphabetically now

--- commands/list.js
+++ commands/list.js
@@ -1,7 +1,10 @@
 const {Audio} = require("../dbObjects.js");
 
 async function listSongs(message, args) {
-    const songs = await Audio.findAll({attributes: ['nickname']});
+    const songs = await Audio.findAll({
+        attributes: ['nickname'], 
+        order:[['nickname','ASC']]
+    });
     const songList = songs.map(s => s.nickname).join(', ') || "No songs.";
     console.log(songList);
     return message.channel.send(`List of songs: ${songList}`, {split:{char:', '}});
